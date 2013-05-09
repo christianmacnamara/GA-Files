5eaa63e41b2bfb2fa918f39b5c8c386cfbd23e47
Christian MacNamara <christian.macnamara@gmail.com>
2013-05-09 15:48:28 -0400

    first commit

---
+++ HW_01.rb
@@ -0,0 +1,70 @@
+###############################################################################
+#
+# Introduction to Ruby on Rails
+#
+# HW 01
+#
+# Purpose:
+#
+# Read the taks below and complete the exercises in this file. We will start
+# to write the beginnings of our "Secret Number Game" using what we've
+# learned in Ruby Lesson 01.
+#
+###############################################################################
+#
+# 1. Read an Interview with the Creator of Ruby
+# 	 http://linuxdevcenter.com/pub/a/linux/2001/11/29/ruby.html
+#
+# 2. Read this Introduction to Programming
+# 	 http://en.wikiversity.org/wiki/Introduction_to_Programming/About_Programming
+#
+# 3. In this file under "Student Solution," print the welcome text of your Secret 
+#    Number Game
+#
+#	(i.e.) "Welcome to the Secret Number Game!"
+#
+# 4. Above your welcome message, write a comment to other coders introducing yourself.
+#	 .
+#
+#
+# 5. Create two new variables, one for your last name, one for your first name
+#    and enter your first and last name as strings.
+#
+#    first_name = ""
+#    last_name = ""
+#
+# 6. Print to the screen that your game was created by you by concating the
+#    first and last name variables.
+#
+#    (i.e.) "Created by " + first_name + " " + last_name
+#
+# 7. Feel free to add more lines of text or add comments to remind you of what
+#    you've learned.
+#
+###############################################################################
+#
+# Student Solution
+
+puts "Hi everyone! My name is Chistian MacNamara. 
+I live in Brooklyn and work in midtown at a consulting firm. I'm excited to learn how to work in Ruby"
+
+puts "Welcome to the Secret Number Game!"
+
+first_name = "Christian"
+last_name = "MacNamara"
+
+puts "Created by " + first_name + " " + last_name
+
+
+# puts "Type in any of the key terms below to see their definition"
+# puts "String, Method, To methods, Array, Variable"
+
+# string = "A string is a set of characters the computer can process."
+# method = "Methods are actions that act upon strings. They are called by placing a '.' in front of the method text"
+# To methods = "Some methods only work on certain types of things. But you can 
+# always convert between different types using Ruby's 'to' methods. To apply a 'To' method, write To_(abbreviation of method)"
+# Array = "Arrays are lists. Lists store things in order"
+# Variable = "Variables save a thing and give it a name. You use the equals sign to do this."
+#
+###############################################################################
+
---
+++ my_first_script.rb
@@ -0,0 +1,12 @@
+puts "Hello World!"
+
+first_name = "Christian"
+last_name = "MacNamara"
+age = 30
+profession = "Consultant"
+
+puts first_name + " " + last_name + " is " + age.to_s 
+
+puts '#{first_name} #{last_name} is #{age}'
+
+puts
